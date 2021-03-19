97881fb656729563706fb210844d0bb0ecd47940
“NAFIS_IQBAL” <nafislr22@gmail.com>
2021-03-19 19:48:29 +0600

    form handlingmethod1

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", home)
+	http.HandleFunc("/request", request)
+	http.HandleFunc("/features", features)
+	http.HandleFunc("/docs", docs)
+	http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("assets"))))
+	http.ListenAndServe(":8888", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+	ptmp, err := template.ParseFiles("template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	ptmp.Execute(w, nil)
+	//fmt.Fprintf(w, `welcome`)
+}
+
+func features(w http.ResponseWriter, r *http.Request) {
+
+	ptmp, err := template.ParseFiles("template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	ptmp, err = ptmp.ParseFiles("wpage/features.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	ptmp.Execute(w, nil)
+	//fmt.Fprintf(w, `welcome`)
+}
+
+func docs(w http.ResponseWriter, r *http.Request) {
+
+	ptmp, err := template.ParseFiles("template/base.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	ptmp, err = ptmp.ParseFiles("wpage/docs.gohtml")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	ptmp.Execute(w, nil)
+	//fmt.Fprintf(w, `welcome`)
+}
+
+func request(w http.ResponseWriter, r *http.Request) {
+
+	//r.ParseForm()
+	name := r.FormValue("name")
+	company := r.FormValue("company")
+	email := r.FormValue("email")
+
+	fmt.Println(name, company, email)
+
+	fmt.Fprintf(w, "recieved %s %s %s", name, company, email) //response
+
+}
